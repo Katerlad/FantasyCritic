d5c2c7f5c526692b50429443fbd7afa4e3c58f06
Stephen Fallon <sfallon92@gmail.com>
2025-05-11 00:24:21 -0400

    Finishing up sql migration script.

--- Database/SchemaUpdates/FC_5.0.2-5.1.0_discordChannelRework.sql
+++ Database/SchemaUpdates/FC_5.0.2-5.1.0_discordChannelRework.sql
@@ -8,6 +8,55 @@ INSERT INTO `tbl_discord_notablemissoptions` (`Name`) VALUES ('InitialScore');
 INSERT INTO `tbl_discord_notablemissoptions` (`Name`) VALUES ('ScoreUpdates');
 INSERT INTO `tbl_discord_notablemissoptions` (`Name`) VALUES ('None');
 
+ALTER TABLE `tbl_discord_gamenewschannel`
+	ADD COLUMN `ShowWillReleaseInYearNews` BIT NULL DEFAULT NULL AFTER `GameNewsSetting`,
+	ADD COLUMN `ShowMightReleaseInYearNews` BIT NULL AFTER `ShowWillReleaseInYearNews`,
+	ADD COLUMN `ShowWillNotReleaseInYearNews` BIT NULL DEFAULT NULL AFTER `ShowMightReleaseInYearNews`,
+	ADD COLUMN `ShowScoreGameNews` BIT NULL DEFAULT NULL AFTER `ShowWillNotReleaseInYearNews`,
+	ADD COLUMN `ShowReleasedGameNews` BIT NULL DEFAULT NULL AFTER `ShowScoreGameNews`,
+	ADD COLUMN `ShowNewGameNews` BIT NULL DEFAULT NULL AFTER `ShowReleasedGameNews`,
+	ADD COLUMN `ShowEditedGameNews` BIT NULL DEFAULT NULL AFTER `ShowNewGameNews`;
+
+UPDATE tbl_discord_gamenewschannel SET
+ShowWillReleaseInYearNews = true,
+ShowMightReleaseInYearNews = true,
+ShowWillNotReleaseInYearNews = true,
+ShowScoreGameNews = true,
+ShowReleasedGameNews = true,
+ShowNewGameNews = true,
+ShowEditedGameNews = true
+WHERE GameNewsSetting = 'All';
+
+UPDATE tbl_discord_gamenewschannel SET
+ShowWillReleaseInYearNews = true,
+ShowMightReleaseInYearNews = true,
+ShowWillNotReleaseInYearNews = false,
+ShowScoreGameNews = true,
+ShowReleasedGameNews = true,
+ShowNewGameNews = true,
+ShowEditedGameNews = true
+WHERE GameNewsSetting = 'MightReleaseInYear';
+
+UPDATE tbl_discord_gamenewschannel SET
+ShowWillReleaseInYearNews = true,
+ShowMightReleaseInYearNews = false,
+ShowWillNotReleaseInYearNews = false,
+ShowScoreGameNews = true,
+ShowReleasedGameNews = true,
+ShowNewGameNews = true,
+ShowEditedGameNews = true
+WHERE GameNewsSetting = 'WillReleaseInYear';
+
+ALTER TABLE `tbl_discord_gamenewschannel`
+	CHANGE COLUMN `ShowWillReleaseInYearNews` `ShowWillReleaseInYearNews` BIT(1) NOT NULL AFTER `ChannelID`,
+	CHANGE COLUMN `ShowMightReleaseInYearNews` `ShowMightReleaseInYearNews` BIT(1) NOT NULL AFTER `ShowWillReleaseInYearNews`,
+	CHANGE COLUMN `ShowWillNotReleaseInYearNews` `ShowWillNotReleaseInYearNews` BIT(1) NOT NULL AFTER `ShowMightReleaseInYearNews`,
+	CHANGE COLUMN `ShowScoreGameNews` `ShowScoreGameNews` BIT(1) NOT NULL AFTER `ShowWillNotReleaseInYearNews`,
+	CHANGE COLUMN `ShowReleasedGameNews` `ShowReleasedGameNews` BIT(1) NOT NULL AFTER `ShowScoreGameNews`,
+	CHANGE COLUMN `ShowNewGameNews` `ShowNewGameNews` BIT(1) NOT NULL AFTER `ShowReleasedGameNews`,
+	CHANGE COLUMN `ShowEditedGameNews` `ShowEditedGameNews` BIT(1) NOT NULL AFTER `ShowNewGameNews`,
+	DROP COLUMN `GameNewsSetting`;
+
 ALTER TABLE `tbl_discord_leaguechannel`
 	ADD COLUMN `ShowPickedGameNews` BIT NULL DEFAULT NULL AFTER `BidAlertRoleID`,
 	ADD COLUMN `ShowEligibleGameNews` BIT NULL DEFAULT NULL AFTER `ShowPickedGameNews`,
@@ -15,10 +64,42 @@ ALTER TABLE `tbl_discord_leaguechannel`
 	ADD COLUMN `NotableMissSetting` VARCHAR(50) NULL DEFAULT NULL AFTER `ShowCurrentYearGameNewsOnly`,
 	ADD CONSTRAINT `FK_tbl_discord_leaguechannel_tbl_discord_notablemissoptions` FOREIGN KEY (`NotableMissSetting`) REFERENCES `tbl_discord_notablemissoptions` (`Name`) ON UPDATE NO ACTION ON DELETE NO ACTION;
 
-ALTER TABLE `tbl_discord_gamenewschannel`
-	ADD COLUMN `ShowMightReleaseInYearNews` BIT NULL AFTER `GameNewsSetting`,
-	ADD COLUMN `ShowWillReleaseInYearNews` BIT NULL DEFAULT NULL AFTER `ShowMightReleaseInYearNews`,
-	ADD COLUMN `ShowScoreGameNews` BIT NULL DEFAULT NULL AFTER `ShowWillReleaseInYearNews`,
-	ADD COLUMN `ShowReleasedGameNews` BIT NULL DEFAULT NULL AFTER `ShowScoreGameNews`,
-	ADD COLUMN `ShowNewGameNews` BIT NULL DEFAULT NULL AFTER `ShowReleasedGameNews`,
-	ADD COLUMN `ShowEditedGameNews` BIT NULL DEFAULT NULL AFTER `ShowNewGameNews`;
+UPDATE tbl_discord_leaguechannel SET
+ShowPickedGameNews = false,
+ShowEligibleGameNews = false,
+ShowCurrentYearGameNewsOnly = false,
+NotableMissSetting = 'None'
+WHERE 
+SendLeagueMasterGameUpdates = 0 AND SendNotableMisses = 0;
+
+UPDATE tbl_discord_leaguechannel SET
+ShowPickedGameNews = false,
+ShowEligibleGameNews = false,
+ShowCurrentYearGameNewsOnly = false,
+NotableMissSetting = 'ScoreUpdates'
+WHERE 
+SendLeagueMasterGameUpdates = 0 AND SendNotableMisses = 1;
+
+UPDATE tbl_discord_leaguechannel SET
+ShowPickedGameNews = true,
+ShowEligibleGameNews = true,
+ShowCurrentYearGameNewsOnly = false,
+NotableMissSetting = 'None'
+WHERE 
+SendLeagueMasterGameUpdates = 1 AND SendNotableMisses = 0;
+
+UPDATE tbl_discord_leaguechannel SET
+ShowPickedGameNews = true,
+ShowEligibleGameNews = true,
+ShowCurrentYearGameNewsOnly = false,
+NotableMissSetting = 'ScoreUpdates'
+WHERE 
+SendLeagueMasterGameUpdates = 1 AND SendNotableMisses = 1;
+
+ALTER TABLE `tbl_discord_leaguechannel`
+	CHANGE COLUMN `ShowPickedGameNews` `ShowPickedGameNews` BIT(1) NOT NULL AFTER `BidAlertRoleID`,
+	CHANGE COLUMN `ShowEligibleGameNews` `ShowEligibleGameNews` BIT(1) NOT NULL AFTER `ShowPickedGameNews`,
+	CHANGE COLUMN `ShowCurrentYearGameNewsOnly` `ShowCurrentYearGameNewsOnly` BIT(1) NOT NULL AFTER `ShowEligibleGameNews`,
+	CHANGE COLUMN `NotableMissSetting` `NotableMissSetting` VARCHAR(50) NOT NULL AFTER `ShowCurrentYearGameNewsOnly`,
+	DROP COLUMN `SendLeagueMasterGameUpdates`,
+	DROP COLUMN `SendNotableMisses`;
